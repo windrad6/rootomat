15682ec24b6de2ff9d36ed99472b54a3aa8b0d63
Manuel Pitz <manuel.pitz@eonerc.rwth-aachen.de>
2021-01-23 19:01:25 +0100

    first working version You have to click calc twice

---
+++ js/plantomat.js
@@ -0,0 +1,104 @@
+
+class plantomat {
+    ctxObj = Object
+    ctx = Object
+    imgUrl = ""
+    selector = Object
+
+    constructor(imgUrl){
+        console.log("start plantomat");
+        this.imgUrl = imgUrl;
+
+    } 
+
+    calc(ctxId) {
+        this.ctxObj = document.getElementById(ctxId);
+       
+        
+        this.loadImage();
+        this.selector = new rectSelect(ctxId);
+
+        var startX = 500;
+        var startY = 1500;
+        var width = 500;
+        var height = 500;
+
+        this.convertCanny(startX, startY, width, height)
+
+        console.log("test");
+    }
+
+    loadImage() {
+        var img = new Image()
+        
+        img.src = this.imgUrl;
+
+        
+        this.ctxObj.width = img.naturalWidth;
+        this.ctxObj.height = img.naturalHeight;    
+
+
+        //draw original to canvas
+        this.ctx = this.ctxObj.getContext('2d');
+        this.ctx.fillStyle = "rgb(0,255,0)";
+        this.ctx.strokeStyle = "rgb(0,255,0)";
+        this.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.naturalWidth, img.naturalHeight);
+
+    }
+
+    
+
+    convertCanny(startX, startY, width, height) {
+
+        var img_u8 = new jsfeat.matrix_t(width, height, jsfeat.U8C1_t);
+       
+    
+        var imageData = this.ctx.getImageData(startX, startY, width, height);
+
+        jsfeat.imgproc.grayscale(imageData.data, width, height, img_u8);
+
+        
+        var r = 5;
+        var kernel_size = (r+1) << 1;
+
+        jsfeat.imgproc.gaussian_blur(img_u8, img_u8, kernel_size, 0);
+
+        jsfeat.imgproc.canny(img_u8, img_u8, 0, 15);
+
+        // render result back to canvas
+        var data_u32 = new Uint32Array(imageData.data.buffer);
+        var alpha = (0xff << 24);
+        var i = img_u8.cols*img_u8.rows, pix = 0;
+        while(--i >= 0) {
+            pix = img_u8.data[i];
+            if(pix != 0){
+                data_u32[i] = alpha;
+            }else
+                data_u32[i] = 0;//alpha | (pix << 16) | (pix << 8) | pix;
+            
+        }
+        this.ctx.putImageData(imageData, startX, startY);
+
+
+    }
+}
+
+class analyzer{
+    constructor(){
+        console.log("new analyzer");
+    }
+}
+
+
+class rectSelect{
+
+    constructor(elmId) {
+        $("#" + elmId).on("click", function(e){alert("test")});
+    }
+
+
+    mousedown() {
+        console.log("mousdown")
+    }
+
+}
