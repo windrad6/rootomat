3836f77411bb2fb354e46973f4245e107804e543
Manuel Pitz <manuel.pitz@eonerc.rwth-aachen.de>
2021-04-18 19:23:49 +0200

    change that -> this

--- js/rectSelect.js
+++ js/rectSelect.js
@@ -35,15 +35,13 @@ class rectSelect{
 
         //check wether it is a touch device or mouse system
         if('ontouchstart' in window  /*|| navigator.maxTouchPoints > 0*/ || navigator.msMaxTouchPoints > 0){
-            var that = this;
-            $("#canvasContainer")[0].addEventListener('touchmove',function(e) {that.mousemove(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset, that);});
-            $("#canvasContainer")[0].addEventListener('touchend',function(e) {that.mouseup(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset, that);});
-            $("#canvasContainer")[0].addEventListener('touchstart',function(e) {that.mousedown(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset, that);});
+            $("#canvasContainer")[0].addEventListener('touchmove',(e) => {that.mousemove(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset);});
+            $("#canvasContainer")[0].addEventListener('touchend',(e) => {this.mouseup(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset);});
+            $("#canvasContainer")[0].addEventListener('touchstart',(e) => {this.mousedown(e.pageX, e.pageY, window.pageXOffset, window.pageYOffset);});
         } else {
-            var that = this;
-            $("#canvasContainer").mousedown(function(e){that.mousedown(e.clientX, e.clientY, 0, 0, that);});
-            $("#canvasContainer").mouseup(function(e){that.mouseup(e.clientX, e.clientY, 0, 0, that);});
-            $("#canvasContainer").mousemove(function(e){that.mousemove(e.clientX, e.clientY, 0, 0, that);});
+            $("#canvasContainer").mousedown((e) => {this.mousedown(e.clientX, e.clientY, 0, 0);});
+            $("#canvasContainer").mouseup((e) => {this.mouseup(e.clientX, e.clientY, 0, 0);});
+            $("#canvasContainer").mousemove((e) => {this.mousemove(e.clientX, e.clientY, 0, 0);});
         }
 
         
@@ -101,89 +99,89 @@ class rectSelect{
         this.rectDomObj.css("height",  + this.size + 'px');
     }
 
-    mousedown(x, y, offsetX, offsetY, that) {
+    mousedown(x, y, offsetX, offsetY) {
 
         if (!this.selfEn)return;
 
-        if (! that.checkRefElmPos(x, y))
+        if (! this.checkRefElmPos(x, y))
             return
 
-        that.rect.x1 = x + $(document).scrollLeft() - offsetX;
-        that.rect.y1 = y + $(document).scrollTop() - offsetY;
+        this.rect.x1 = x + $(document).scrollLeft() - offsetX;
+        this.rect.y1 = y + $(document).scrollTop() - offsetY;
 
 
-        if(that.size != -1)
-            that.calcCircle()
+        if(this.size != -1)
+            this.calcCircle()
         else
-            that.calcDiv();
-        that.rectDomObj.show();
+            this.calcDiv();
+        this.rectDomObj.show();
 
 
     }
 
-    mouseup(x, y, offsetX, offsetY, that) {
+    mouseup(x, y, offsetX, offsetY) {
         if (!this.selfEn)return;
 
-        if (! that.checkRefElmPos(x, y))
+        if (! this.checkRefElmPos(x, y))
             return
 
-        if(that.mouseupFcn !== null) {
+        if(this.mouseupFcn !== null) {
             //correct if square is not done left to right
-            if (that.rect.x1 > that.rect.x2) {
-                var tmp = that.rect.x1;
-                that.rect.x1 = that.rect.x2;
-                that.rect.x2 = tmp;
+            if (this.rect.x1 > this.rect.x2) {
+                var tmp = this.rect.x1;
+                this.rect.x1 = this.rect.x2;
+                this.rect.x2 = tmp;
             }
             //correct if square is not done top to bottom
-            if (that.rect.y1 > that.rect.y2) {
-                var tmp = that.rect.y1;
-                that.rect.y1 = that.rect.y2;
-                that.rect.y2 = tmp;
+            if (this.rect.y1 > this.rect.y2) {
+                var tmp = this.rect.y1;
+                this.rect.y1 = this.rect.y2;
+                this.rect.y2 = tmp;
             }
 
             //correct the 0 point of the rectangle to be relative to the elmId
             var coordinates = {"x1" : 0,"x2" : 0,"y1" : 0,"y2" : 0}
-            if (that.size != -1) {
-                coordinates.x1 = that.rect.x2 - that.size/2  - that.refElm.x1
-                coordinates.y1 = that.rect.y2 - that.size/2 - that.refElm.y1
-                coordinates.x2 = that.rect.x2 + that.size/2 - that.refElm.x1
-                coordinates.y2 = that.rect.y2 + that.size/2 - that.refElm.y1
+            if (this.size != -1) {
+                coordinates.x1 = this.rect.x2 - this.size/2  - this.refElm.x1
+                coordinates.y1 = this.rect.y2 - this.size/2 - this.refElm.y1
+                coordinates.x2 = this.rect.x2 + this.size/2 - this.refElm.x1
+                coordinates.y2 = this.rect.y2 + this.size/2 - this.refElm.y1
             } else {
-                coordinates.x1 = that.rect.x1 - that.refElm.x1
-                coordinates.x2 = that.rect.x2 - that.refElm.x1
-                coordinates.y1 = that.rect.y1 - that.refElm.y1
-                coordinates.y2 = that.rect.y2 - that.refElm.y1
+                coordinates.x1 = this.rect.x1 - this.refElm.x1
+                coordinates.x2 = this.rect.x2 - this.refElm.x1
+                coordinates.y1 = this.rect.y1 - this.refElm.y1
+                coordinates.y2 = this.rect.y2 - this.refElm.y1
             }
 
-            that.mouseupFcn.ptr(coordinates, that.mouseupFcn.scope);
+            this.mouseupFcn.ptr(coordinates, this.mouseupFcn.scope);
         }
 
-        that.rect = {"x1" : 0, "x2" : 0, "y1" : 0, "y2" : 0};
-        that.rectDomObj.hide();
+        this.rect = {"x1" : 0, "x2" : 0, "y1" : 0, "y2" : 0};
+        this.rectDomObj.hide();
     }
 
-    mousemove(x, y, offsetX, offsetY, that) {
+    mousemove(x, y, offsetX, offsetY) {
         if (!this.selfEn)return;
 
-        if (!that.checkRefElmPos(x, y))
+        if (!this.checkRefElmPos(x, y))
             return;
 
-        if(that.rect.y1 == 0 || that.rect.x1 == 0)
+        if(this.rect.y1 == 0 || this.rect.x1 == 0)
             return;
 
-        that.rect.x2 = x + $(document).scrollLeft() - offsetX;
-        that.rect.y2 = y + $(document).scrollTop() - offsetY;
-        if(that.size != -1) {
+        this.rect.x2 = x + $(document).scrollLeft() - offsetX;
+        this.rect.y2 = y + $(document).scrollTop() - offsetY;
+        if(this.size != -1) {
             var coordinates = {"x1" : 0,"x2" : 0,"y1" : 0,"y2" : 0}
             
-            coordinates.x1 = that.rect.x2 - that.size/2  - that.refElm.x1
-            coordinates.y1 = that.rect.y2 - that.size/2 - that.refElm.y1
-            coordinates.x2 = that.rect.x2 + that.size/2 - that.refElm.x1
-            coordinates.y2 = that.rect.y2 + that.size/2 - that.refElm.y1
-            that.mouseupFcn.ptr(coordinates, that.mouseupFcn.scope);
-            that.calcCircle()
+            coordinates.x1 = this.rect.x2 - this.size/2  - this.refElm.x1
+            coordinates.y1 = this.rect.y2 - this.size/2 - this.refElm.y1
+            coordinates.x2 = this.rect.x2 + this.size/2 - this.refElm.x1
+            coordinates.y2 = this.rect.y2 + this.size/2 - this.refElm.y1
+            this.mouseupFcn.ptr(coordinates, this.mouseupFcn.scope);
+            this.calcCircle()
         } else {
-            that.calcDiv();
+            this.calcDiv();
 
         }
             
